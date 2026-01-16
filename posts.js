81713a0d1b497ad5b2c4a4567ee3450c0ac223b2
tang361313182@outlook.com <tang361313182@outlook.com>
2026-01-16 01:15:04 -0800

    cal_diary

---
+++ posts.js
@@ -0,0 +1,13 @@
+var articleList = [
+    { "title": "cal_diary", "category": "Diary" },
+    { "title": "california_diary", "category": "Diary" },
+    { "title": "tech_note", "category": "Tech" },
+    { "title": "vintage_style_share", "category": "Share" },
+    { "title": "8bit_design", "category": "Design" },
+
+    { "title": "first_post", "category": "Essay" },
+    { "title": "california_diary", "category": "Diary" },
+    { "title": "tech_note", "category": "Tech" },
+    { "title": "vintage_style_share", "category": "Share" },
+    { "title": "8bit_design", "category": "Design" }
+];
