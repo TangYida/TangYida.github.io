5881f1a4c9095199a98891a0535e050b098fbe66
tang361313182@outlook.com <tang361313182@outlook.com>
2026-02-15 16:03:34 -0800

    The_False_Spring

--- posts.js
+++ posts.js
@@ -1,13 +1,3 @@
 var articleList = [
-    { "title": "cal_diary", "category": "Diary" },
-    { "title": "california_diary", "category": "Diary" },
-    { "title": "tech_note", "category": "Tech" },
-    { "title": "vintage_style_share", "category": "Share" },
-    { "title": "8bit_design", "category": "Design" },
-
-    { "title": "first_post", "category": "Essay" },
-    { "title": "california_diary", "category": "Diary" },
-    { "title": "tech_note", "category": "Tech" },
-    { "title": "vintage_style_share", "category": "Share" },
-    { "title": "8bit_design", "category": "Design" }
+    { "title": "The_False_Spring", "category": "Bullshit" },
 ];
