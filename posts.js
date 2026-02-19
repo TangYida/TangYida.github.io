f1aec25b02008c4cb8b10f31a27a96f6b52cc485
tang361313182@outlook.com <tang361313182@outlook.com>
2026-02-19 11:21:28 -0800

    ddd

--- posts.js
+++ posts.js
@@ -1,4 +1,5 @@
 var articleList = [
+    { "title": "map", "category": "Project" },
     { "title": "The_False_Spring", "category": "Bullshit" },
 	{"title": "Philosophical_Remarks", "category": "Mystery"}
 ];
