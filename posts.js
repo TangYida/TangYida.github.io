31f5e5b341bc0862e2c1098e03de8a6bfaac7a68
tang361313182@outlook.com <tang361313182@outlook.com>
2026-04-11 16:04:27 +0800

    OMG_This_is_a_whole_different_person

--- posts.js
+++ posts.js
@@ -1,5 +1,6 @@
 var articleList = [
+    { "title": "OMG_This_is_a_whole_different_person", "category": "Bullshit" },
     { "title": "map", "category": "Project" },
     { "title": "The_False_Spring", "category": "Bullshit" },
-	{"title": "Philosophical_Remarks", "category": "Mystery"}
+    {"title": "Philosophical_Remarks", "category": "Mystery"}
 ];
